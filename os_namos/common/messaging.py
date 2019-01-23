a8d4b58a65f8ef166c862347db8cf07c2557c103
Hervé Beraud <hberaud@redhat.com>
2019-01-23 10:13:36 +0100

    Remove usage of oslo.messaging deprecated transport method.
    Since few months oslo.messaging project have deprecated
    the get_transport method from the transport layer module.
    
    Move get_transport to get_rpc_transport.
    
    https://bugs.launchpad.net/cloud-archive/+bug/1714945
    
    Related-Bug: #1714945
    
    Change-Id: I66ed75902d43ae530328950cfbd06a9e8ea2d0f8

--- os_namos/common/messaging.py
+++ os_namos/common/messaging.py
@@ -64,8 +64,7 @@ def get_transport(url=None, optional=False, cache=True):
     transport = TRANSPORTS.get(cache_key)
     if not transport or not cache:
         try:
-            transport = oslo_messaging.get_transport(cfg.CONF, url,
-                                                     aliases=_ALIASES)
+            transport = oslo_messaging.get_rpc_transport(cfg.CONF, url)
         except oslo_messaging.InvalidTransportURL as e:
             if not optional or e.url:
                 # NOTE(sileht): olso.messaging is configured but unloadable
